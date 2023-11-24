99b4f32849f1f8cb301720ef6e6549f1a9f4fc27
saipepu <saipepu.mdy257@gmail.com>
2023-11-25 00:13:33 +0700

    add react-hot-toast

--- app/layout.tsx
+++ app/layout.tsx
@@ -2,6 +2,7 @@ import type { Metadata } from 'next'
 import { Inter } from 'next/font/google'
 import './globals.css'
 import AuthContext from './context/AuthContext'
+import ToasterContext from './context/ToasterContext'
 
 const inter = Inter({ subsets: ['latin'] })
 
@@ -19,6 +20,7 @@ export default function RootLayout({
     <html lang="en">
       <body className={inter.className}>
         <AuthContext>
+          <ToasterContext />
           {children}
         </AuthContext>
       </body>
